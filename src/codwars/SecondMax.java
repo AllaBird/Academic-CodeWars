837f901f73c3944bc0c4d547a8b00416b1c4ee24
AllaBird <allavorobieva@yahoo.com>
2026-01-24 16:29:25 -0500

    Add SecondMax task

---
+++ src/codwars/SecondMax.java
@@ -0,0 +1,27 @@
+package codwars;
+
+import java.util.List;
+
+public class SecondMax {
+
+           public static int secondMax(List<Integer> numbers) {
+            int max = Integer.MIN_VALUE;
+            int secondMax = Integer.MIN_VALUE;
+
+               for (int num : numbers) {
+                   if (num > max) {
+                       secondMax = max;
+                       max = num;
+                   } else if (num > secondMax) {
+                       secondMax = num;
+                   }
+               }
+
+            return secondMax;
+        }
+
+        public static void main(String[] args) {
+            System.out.println(secondMax(List.of(1, 15, 4 ,6 ,7, 9)));
+        }
+    }
+
