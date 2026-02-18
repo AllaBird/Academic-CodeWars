cd6f625ea53b9deeb6dd49d7526be90c865f4583
AllaBird <allavorobieva@yahoo.com>
2026-02-17 20:42:28 -0500

    Add  RemoveDuplicateCharacters

---
+++ src/codwars/RemoveDuplicateCharacters.java
@@ -0,0 +1,20 @@
+package codwars;
+
+public class RemoveDuplicateCharacters {
+
+    public static String removeDuplicates(String str) {
+        StringBuilder result = new StringBuilder();
+
+        for (char c : str.toCharArray()) {
+            if (result.indexOf(String.valueOf(c)) == -1) {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates("hello"));
+    }
+}
